129afb7368cb66e80a7991918859870a5cbf4fdc
Shevchik <Shev4ik.den@gmail.com>
2018-04-26 11:01:05 +0300

    User PlayerLoginFinish event for protocolsupport

--- src/skinsrestorer/bukkit/listeners/ProtocolSupportListener.java
+++ src/skinsrestorer/bukkit/listeners/ProtocolSupportListener.java
@@ -3,8 +3,8 @@ package skinsrestorer.bukkit.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import protocolsupport.api.events.PlayerPropertiesResolveEvent;
-import protocolsupport.api.events.PlayerPropertiesResolveEvent.ProfileProperty;
+import protocolsupport.api.events.PlayerLoginFinishEvent;
+import protocolsupport.api.utils.ProfileProperty;
 import skinsrestorer.bukkit.SkinsRestorer;
 import skinsrestorer.shared.format.SkinProfile;
 import skinsrestorer.shared.storage.SkinStorage;
@@ -13,7 +13,7 @@ import skinsrestorer.shared.utils.SkinFetchUtils.SkinFetchFailedException;
 public class ProtocolSupportListener implements Listener {
 
 	@EventHandler
-	public void onSkinResolve(PlayerPropertiesResolveEvent event) {
+	public void onSkinResolve(PlayerLoginFinishEvent event) {
 		String name = event.getName();
 		SkinProfile skinprofile = SkinStorage.getInstance().getOrCreateSkinData(name);
 		try {
@@ -23,7 +23,8 @@ public class ProtocolSupportListener implements Listener {
 		}
 		skinprofile.applySkin(property -> {
 			String propertyname = property.getName();
-			if (!event.hasProperty(propertyname) || skinprofile.isForced()) {
+			if (!event.hasProperties(propertyname) || skinprofile.isForced()) {
+				event.removeProperties(propertyname);
 				event.addProperty(new ProfileProperty(propertyname, property.getValue(), property.getSignature()));
 			}
 		}); 
